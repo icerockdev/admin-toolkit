b68dc623465d849c7d1b589abc613d07b8990a46
Fedor Katurov <gotham48@gmail.com>
2020-03-27 13:50:12 +0700

    fixed docs

---
+++ docs/precache-manifest.b10eafd126fb11ab39eae8553fb3027f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf715018d9ad0fc5e1c697e21ac07ba4",
+    "url": "/admin-toolkit/index.html"
+  },
+  {
+    "revision": "6f96aac754e3e28c99ac",
+    "url": "/admin-toolkit/static/js/2.486487a0.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/admin-toolkit/static/js/2.486487a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a56e6b880a6cc032157",
+    "url": "/admin-toolkit/static/js/main.6e7ff10d.chunk.js"
+  },
+  {
+    "revision": "2ee4b0ecb83607246145",
+    "url": "/admin-toolkit/static/js/runtime-main.eeed6b69.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8093ca050ed754789924adb9fb147d33.js"
+  "/admin-toolkit/precache-manifest.b10eafd126fb11ab39eae8553fb3027f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/admin-toolkit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
