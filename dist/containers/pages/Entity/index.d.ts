52466fed1efac079ba70176b66cdfdb7301c16b5
Fedor Katurov <gotham48@gmail.com>
2020-03-27 12:02:55 +0700

    copyright for typedefs

--- dist/containers/pages/Entity/index.d.ts
+++ dist/containers/pages/Entity/index.d.ts
@@ -1,3 +1,5 @@
+/* Copyright (c) 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license. */
+
 /// <reference types="react" />
 import { IEntityProps } from '../../../types/entity';
 import { Page } from '../Page';
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
+/* Copyright (c) 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license. */
+
 export * from './containers/pages';
 export * from './types/auth';
 export * from './types/config';
