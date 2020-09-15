5b7b4eb2fc4d07c9e8341328d0e5221d312607d2
Fedor Katurov <gotham48@gmail.com>
2020-09-15 15:38:40 +0700

    WD-350 added create and export buttons

--- src/application/modules/pages/CrudlEntity/components/renderers/list/CrudlListButtons/index.tsx
+++ src/application/modules/pages/CrudlEntity/components/renderers/list/CrudlListButtons/index.tsx
@@ -3,11 +3,33 @@ import { CrudlListRendererProps } from '~/application/modules/pages/CrudlEntity/
 import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { observer } from 'mobx-react';
+import { useEntity } from '~/utils/hooks';
+import { Button } from '@material-ui/core';
+import { ImportExport, NoteAdd } from '@material-ui/icons';
+import { CrudlActionEnum } from '~/application/modules/pages/CrudlEntity/types';
 
-const CrudlListButtons: CrudlListRendererProps['buttons'] = observer(() => (
-  <div className={classNames(styles.buttons, 'crudl-list__buttons')}>
-    BUTTONS
-  </div>
-));
+const CrudlListButtons: CrudlListRendererProps['buttons'] = observer(() => {
+  const entity = useEntity();
+
+  const { create, export: exp } = entity.features;
+
+  return (
+    <div className={classNames(styles.buttons, 'crudl-list__buttons')}>
+      {(exp || true) && (
+        <Button variant="outlined" color="primary">
+          <ImportExport />
+        </Button>
+      )}
+
+      {create && (
+        <form action={`${entity.url}/${CrudlActionEnum.create}`}>
+          <Button variant="contained" color="primary" type="submit">
+            <NoteAdd />
+          </Button>
+        </form>
+      )}
+    </div>
+  );
+});
 
 export { CrudlListButtons };
