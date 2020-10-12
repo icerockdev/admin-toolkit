97392bb0e0c6cf99994a1e439ae6f4a013d9488b
Fedor Katurov <gotham48@gmail.com>
2020-10-12 12:36:42 +0700

    WD-350 added comments

--- src/application/modules/pages/Feature/items/FeatureData/index.ts
+++ src/application/modules/pages/Feature/items/FeatureData/index.ts
@@ -1,24 +1,45 @@
-import { action, computed, extendObservable, observable, toJS } from 'mobx';
+import { action, computed, extendObservable, observable } from 'mobx';
 import { FeatureDataReference } from '~/application/modules/pages/Feature/types/reference';
 import { FeatureApiReferences } from '~/application/modules/pages/Feature/types';
 import { Feature } from '~/application/modules/pages/Feature';
-import { has, pickBy } from 'ramda';
+import { pickBy } from 'ramda';
 
 export class FeatureData<
   Fields extends Record<string, any> = Record<string, any>
 > {
   constructor(private feature: Feature<Fields>) {}
 
+  /**
+   * FeatureApi's references dictionary (see this.createReferenceData)
+   */
   @observable references: Record<string, FeatureDataReference> = {};
 
   @observable isLoading: boolean = true;
 
+  /**
+   * Values for list
+   */
   @observable list: Fields[] = [];
+
+  /**
+   * Values for single item display
+   */
   @observable read: Partial<Record<keyof Fields, Fields[keyof Fields]>> = {};
+
+  /**
+   * Current editor values, copied from read before create/update form appears
+   */
   @observable editor: Partial<Record<keyof Fields, Fields[keyof Fields]>> = {};
 
+  /**
+   * Validation error dictionary
+   */
   @observable errors: Partial<Record<keyof Fields, string>> = {};
 
+  /**
+   * Creates editor[fieldname] reference instance for refs from FeatureApi
+   * @param refs
+   */
   @action
   createReferenceData(refs: FeatureApiReferences<Fields> = {}) {
     // fills this.references with fields from Feature
@@ -39,6 +60,9 @@ export class FeatureData<
     this.editor = {};
   }
 
+  /**
+   * Sets editor data from current item data, fetched from backend
+   */
   @action
   copyReadToEditor() {
     this.isLoading = true;
@@ -59,6 +83,10 @@ export class FeatureData<
     };
   }
 
+  /**
+   * Data fields, only for current mode (update/create), based on field and feature
+   * roles and permissions
+   */
   @computed
   get editorDataForCurrentMode() {
     const fields = this.feature.fieldsOfCurrentMode.map((field) => field.name);
