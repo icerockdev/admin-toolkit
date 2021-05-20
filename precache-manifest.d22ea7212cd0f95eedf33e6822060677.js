24a05d888409d912fb1067ab4eac17f5b893ee74
YokiToki <YokiToki@users.noreply.github.com>
2021-05-20 04:02:46 +0000

    deploy: fcb5c27fd4b8f5ec9705cd2871c6b40ab56379bb

---
+++ precache-manifest.d22ea7212cd0f95eedf33e6822060677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28601769149852e23bb24c34cc8e48af",
+    "url": "/admin-toolkit/index.html"
+  },
+  {
+    "revision": "ed307e2d1e6fe2005b50",
+    "url": "/admin-toolkit/static/css/main.b06a8015.chunk.css"
+  },
+  {
+    "revision": "3c7d19978574d120c608",
+    "url": "/admin-toolkit/static/js/2.b5ff2f92.chunk.js"
+  },
+  {
+    "revision": "1cb78f93da9496192179d734ee8f5c08",
+    "url": "/admin-toolkit/static/js/2.b5ff2f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed307e2d1e6fe2005b50",
+    "url": "/admin-toolkit/static/js/main.3faf8dc6.chunk.js"
+  },
+  {
+    "revision": "ccff2c0f31a99e12cfde",
+    "url": "/admin-toolkit/static/js/runtime-main.aad961bc.js"
+  }
+]);
